6c666d69bd0d3b4abc30f72f0b1005de596d80bc
ORB-Union <take5079@gmail.com>
2017-04-23 23:08:11 +0900

    유니티 UI 작업 2
    UI 인터렉션 공부중

---
+++ UI Tutorial 2/Assets/Scripts/Start.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Start : MonoBehaviour {
+	
+	void Awake() {
+        
+    }
+
+    public void StartButton()
+    {
+        Invoke("startgame", 3f);        
+        //invoke 함수는 startgame 이라는 함수를 실행시켜주는데 몇초뒤(3f는 3초뒤)에 실행 시켜줄거냐 이다.
+    }
+
+    void startgame()
+    {
+        Application.LoadLevel("InGame Zeta");
+    }
+
+
+    public void optionbutton()
+    {
+        Invoke("OptionGame", 0f);
+        //invoke 함수는 startgame 이라는 함수를 실행시켜주는데 몇초뒤(3f는 3초뒤)에 실행 시켜줄거냐 이다.
+    }
+
+    void OptionGame()
+    {
+        Application.LoadLevel("InGame");
+    }
+
+    public void TitleButton()
+    {
+        Invoke("MainTitleGame", 0f);
+    }
+
+    void MainTitleGame()
+    {
+        Application.LoadLevel("MainTitle");
+    
+    }
+}
---
+++ UI Tutorial 2/Assets/ZetaScene/ZetaControl.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZetaControl : MonoBehaviour {
+
+    protected Animator animator;
+    private float directionX = 0;
+    private float directionY = 0;
+    private bool walking = false;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        if (animator)
+        {
+            float h = Input.GetAxisRaw("Horizontal");
+            float v = Input.GetAxisRaw("Vertical");
+            walking = true;
+            if (h > 0)
+            {
+                directionX = 1;
+                directionY = 0;
+                GetComponent<AudioSource>().UnPause();
+            }
+            else if(h < 0) {
+                directionX = -1;
+                directionY = 0;
+                GetComponent<AudioSource>().UnPause();
+            } else if (v > 0) {
+                directionX = 0;
+                directionY = 1;
+                GetComponent<AudioSource>().UnPause();
+            } else if (v < 0) {
+                directionX = 0;
+                directionY = -1;
+                GetComponent<AudioSource>().UnPause();
+            } else {
+                walking = false;
+                GetComponent<AudioSource>().Pause();
+            }
+            if (walking)
+            {
+                transform.Translate(new Vector3(directionX, directionY, 0) * Time.deltaTime * 0.5f);
+            }
+            animator.SetFloat("DirectionX", directionX);
+            animator.SetFloat("DirectionY", directionY);
+            animator.SetBool("Walking", walking);
+        }
+    }
+}
